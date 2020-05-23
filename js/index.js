e50c80b503d6ac53f74c5c8fafb921ffc5fe4a9d
Anirudh Kachroo <anirudh.kac@yahoo.com>
2020-05-23 19:54:34 +0530

    add when fate fits

--- js/index.js
+++ js/index.js
@@ -1,5 +1,20 @@
+// animating subheading
 new TypeIt("#sub-head", {
     strings:"",
     speed: 120,
     loop: true,
   }).go();
+
+  let book = document.querySelector(".book-cover");
+  book.front  =true;
+  book.addEventListener("click",function(){
+    //this.classList.toggle("book-bigger")
+    if(this.front){
+      this.src = "/img/bookcoverback.jpg";
+      this.front = false;
+    }else{
+      this.src = "/img/bookcover.jpg";
+      this.front = true;
+    }
+    
+  })
